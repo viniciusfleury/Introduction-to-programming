69c33b2361086ce73726c48bea0cbfd24274fa97
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-11-03 16:05:00 -0300

    Create Estrutura-Basica.c

---
+++ Estrutura-Basica.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //library declaration
+
+int main(){ //main program
+  int a,b,c; //Declaring variables
+  
+  a = 2;
+  b = 3;
+  c = a + b;
+  
+  printf(“%d\n”,c);  //Structure for message output
+  return 0; //main ending
+}
